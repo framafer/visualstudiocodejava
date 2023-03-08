c388e10ec6368aa250e5dd8b3527234d045b1952
framafer <framafer@gmail.com>
2023-03-08 20:46:49 +0100

    world_4

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! 3");
+        System.out.println("Hello, World! 4");
     }
 }
