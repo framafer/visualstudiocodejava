6f9072a11123b52879eac74e7a018cf1805092a6
framafer <framafer@gmail.com>
2023-03-08 21:06:31 +0100

    cambio a rama2

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! 5");
-        System.out.println("hola mundo");
+        System.out.println("hola mundo rama2");
     }
 }
